7c6afd8d0c3e9c8b4e51c9cefe047762fcefd4db
Trexter <sheridak@purdue.edu>
2016-09-21 23:20:02 -0400

    Restructuring layout of pauvsi_m7. Each section will now get its own package. I have also started rewriting the driver and calibrator.

---
+++ include/pgc_driver/Driver.h
@@ -0,0 +1,32 @@
+/*
+ * Driver.h
+ *
+ *  Created on: Sep 21, 2016
+ *      Author: kevinsheridan
+ */
+
+#ifndef PAUVSI_M7_PAUVSI_PGC_DRIVER_INCLUDE_PGC_DRIVER_DRIVER_H_
+#define PAUVSI_M7_PAUVSI_PGC_DRIVER_INCLUDE_PGC_DRIVER_DRIVER_H_
+
+#include <ros/ros.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d.hpp>
+#include <iostream>
+#include <string>
+#include <math.h>
+
+
+class Driver
+{
+private:
+	cv::Mat rawImage;
+public:
+	Driver(){
+
+	}
+};
+
+
+
+#endif /* PAUVSI_M7_PAUVSI_PGC_DRIVER_INCLUDE_PGC_DRIVER_DRIVER_H_ */
---
+++ src/pgc_driver.cpp
@@ -0,0 +1,9 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+
+int main(int argc, char **argv)
+{
+
+	return 0;
+}
