01f87e3e43d2864f6a531b62a373e4ac8db83b9e
Trexter <sheridak@purdue.edu>
2016-09-25 23:21:00 -0400

    added the flycap library. added vo.launch

---
+++ install_flycapture.sh
@@ -0,0 +1,9 @@
+# INSTALL FlyCapture2 DEPENDENCIES
+sudo apt-get install libraw1394-11 libgtkmm-2.4-dev libglademm-2.4-dev libgtkglextmm-x11-1.2-dev libgtkglextmm-x11-1.2 libusb-1.0-0
+
+#installs the flycapture library
+roscd pauvsi_pgc_driver/FlyCapture/flycapture2.2
+sudo sh install_flycapture.sh
+cd
+
+
