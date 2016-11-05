40385775560bbefd8d612d9a367f9a931e4f0555
moore585 <moore585@purdue.edu>
2016-11-04 22:17:40 -0400

    renamed color_tracker to pauvsi_tracking

--- install_flycapture.sh
+++ install_flycapture.sh
@@ -2,5 +2,7 @@
 sudo apt-get install libraw1394-11 libgtkmm-2.4-dev libglademm-2.4-dev libgtkglextmm-x11-1.2-dev libgtkglextmm-x11-1.2 libusb-1.0-0
 apt-get -f install
 
+pushd .
 cd FlyCapture/flycapture2.2
 sudo sh install_flycapture.sh
+popd
